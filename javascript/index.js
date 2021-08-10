0351cb6a54c221e29053dfa7e2981d21fa353b02
Rohit Bhatt <rohitsharma80449@gmail.com>
2021-08-10 16:12:24 +0530

    first file javascript

---
+++ javascript/index.js
@@ -0,0 +1,33 @@
+// 1
+let student = ["aniket" ,"rohit", "bantu", "monty","rathor", "nikhil"];
+console.log(student);
+
+//2
+console.log(student[0]);
+
+//3
+student.splice(5,1, "Anshuman");
+
+console.log(student)
+//4
+var firstStudent = student[0]
+console.log(firstStudent)
+
+//5
+student.push("govind")
+console.log(student);
+//6
+student.unshift("amit")
+console.log(firstStudent);
+
+//7
+student.length;
+//8
+student.pop();
+//9
+student.unshift();
+//10
+console.log(student.length)
+
+//finally
+
